1717e3f31182361b280e9a7671eb56da2954ab07
calebyhan <calebhantech@gmail.com>
2024-08-19 12:58:30 -0400

    test commit

--- welcome.py
+++ welcome.py
@@ -1,6 +1,6 @@
 """A welcoming test program to start COMP110"""
 
-__author__ = "01234567"
+__author__ = "730738108"
 
 print("Welcome to COMP110!")
 print("You are in for a fun adventure into programming!")
