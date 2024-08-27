f9e8fafac9473e240db858917b949eba785fc18f
calebyhan <calebhantech@gmail.com>
2024-08-27 09:41:22 -0400

    commit

---
+++ cqs/cq00_functions.py
@@ -0,0 +1,16 @@
+"""Functions Challenge"""
+
+__author__: str = "730738108"
+
+
+def mimic(message: str) -> str:
+    """
+    Returns the inputted message
+
+    :param message: message to mimic back
+    """
+    return message
+
+
+if __name__ == "__main__":
+    print(mimic(message=input("What is your message?")))
