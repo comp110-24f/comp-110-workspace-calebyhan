b8925ae28350b7157741202d4d253e91e94cac62
calebyhan <calebhantech@gmail.com>
2024-09-11 11:58:25 -0400

    commit

---
+++ lessons/cl08.py
@@ -0,0 +1,12 @@
+def get_weather_report() -> str:
+    weather: str = input("What is the weather? ")
+    if weather == "rainy" or weather == "cold":
+        print("Bring a jacket!")
+    elif weather == "hot":
+        print("Keep cool out there!")
+    else:
+        print("I don't recognize this weather.")
+    return weather
+
+
+get_weather_report()
